9cfb7bc9a41b6ce55b4c544ca23a774b450a4f31
Vitaly <vitalic.web@gmail.com>
2023-04-25 20:47:46 +0300

    Add YummyMeal component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <h1>{{ name }}</h1>
   <button @click="placeOrder">Place Order</button>
+  <YummyMeal name="Hamburger" :price="5" @addToCart="addItemToCart" />
 </template>
 
 <script>
+import YummyMeal from './components/YummyMeal.vue';
+
 export default {
+  components: {
+    YummyMeal,
+  },
   setup() {
     const name = 'The Snazzy Burger';
+
     const placeOrder = () => alert('You re order has been placed!');
+    const addItemToCart = (item) => alert(`One ${item} added to the cart!`);
 
     return {
       name,
       placeOrder,
+      addItemToCart,
     }
   }
 }
